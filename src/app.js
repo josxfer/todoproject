da56e1f546b5985de83223e539780a9837220552
Josxfer <josxfer@gmail.com>
2023-09-27 19:11:22 -0500

    Añadiendo rutas de tareas y de usuario

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 import express from "express";
 import morgan from "morgan";
+import taskRoutes from "./routes/task.routes.js";
+import authroutes from "./routes/auth.routes.js";
 
 const app = express();
 
+//Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//Routes
 app.get("/", (req, res) => res.json({ message: "Bienvenido a mi API" }));
+app.use('/api', taskRoutes);
+app.use('/api', authroutes);
 
+//Error handler
 app.use((err, req, res, next) => {
   res.status(500).json({
     status: "error",
