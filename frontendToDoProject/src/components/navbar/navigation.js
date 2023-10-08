691f076e1219a6b859e0575960a9e4bc7a89deeb
Josxfer <josxfer@gmail.com>
2023-10-07 23:56:21 -0500

    Se agrega barra de navegación con estilos y redireccionando a las diferentes rutas

---
+++ frontendToDoProject/src/components/navbar/navigation.js
@@ -0,0 +1,30 @@
+export const navigation = [
+  {
+    name: "Iniciar sesión",
+    path: "/login",
+  },
+  {
+    name: "Regístrate",
+    path: "/register",
+  },
+  {
+    name: "Acerca de",
+    path: "/about",
+  },
+  {
+    name: "Tareas",
+    path: "/tasks",
+  },
+  {
+    name: "Nueva tarea",
+    path: "/tasks/new",
+  },
+  {
+    name: "Editar tarea",
+    path: "/tasks/1/edit",
+  },
+  {
+    name: "Perfil de usuario",
+    path: "/profile",
+  },
+];
--- frontendToDoProject/src/components/ui/index.js
+++ frontendToDoProject/src/components/ui/index.js
@@ -1,5 +1,5 @@
-export {Button} from "./Button";
-export {Card} from "./Card";
-export {Input} from "./Input";
-export {Label} from "./Label";
-
+export { Button } from "./Button";
+export { Card } from "./Card";
+export { Input } from "./Input";
+export { Label } from "./Label";
+export { Container } from "./container";
