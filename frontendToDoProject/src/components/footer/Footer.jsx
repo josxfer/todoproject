271e84038674f7c88a4ba6e4b81d0c78dd28ee52
Josxfer <josxfer@gmail.com>
2023-10-09 15:28:15 -0500

    Proyecto finalizado listo para produccion

--- frontendToDoProject/src/components/footer/Footer.jsx
+++ frontendToDoProject/src/components/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import { Container } from "../ui";
+import { AiFillHeart } from "react-icons/ai";
 
 function Footer() {
   return (
@@ -6,7 +7,7 @@ function Footer() {
       <Container className="flex justify-between py-3">
         <h1 className="flex font-light text-slate-300">Proyecto final para Dev.F</h1>
         <ul className="flex items-center justify-center md:gap-x-1 font-light text-slate-300">
-          <li>Presentado por Jose Fernando Espinosa</li>
+          <li className="flex items-center" >Hecho con<AiFillHeart className="flex text-red-600" />por Jose Fernando Espinosa</li>
           <li className="flex gap-x-1 items-center justify-center font-light text-slate-300"> - 2023</li>
         </ul>
       </Container>
--- frontendToDoProject/src/components/ui/Textarea.jsx
+++ frontendToDoProject/src/components/ui/Textarea.jsx
@@ -3,7 +3,7 @@ import { forwardRef } from "react";
 export const Textarea = forwardRef((props, ref) => {
   return (
     <textarea
-      className="bg-zinc-800 px-3 py-2 block my-2 w-full"
+      className="bg-purple-800 px-3 py-2 block my-2 w-full rounded-md"
       ref={ref}
       {...props}
     >
